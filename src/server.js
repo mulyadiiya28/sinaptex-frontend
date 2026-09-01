335ddb82b8cc4b62ff510521580a31f010d3c7b8
mulyadiiya28 <info.universal.mcs@gmail.com>
2026-09-01 21:29:14 +0700

    Add server.js to create HTTP server with Next.js

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// server.js
+const { createServer } = require('http');
+const next = require('next');
+
+const port = process.env.PORT || 3000;
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, '0.0.0.0', () => {
+    console.log(`> Ready on port ${port}`);
+  });
+});
