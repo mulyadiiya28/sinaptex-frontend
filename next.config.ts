7b533bf58208f1aaee4d74e51743ec228c6f7af9
mulyadiiya28 <info.universal.mcs@gmail.com>
2026-09-01 22:52:23 +0700

    Update service worker configuration to disable in dev

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  disable: false,
+  disable: process.env.NODE_ENV !== "production",
   reloadOnOnline: true,
 });
 
